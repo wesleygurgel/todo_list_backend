2dff9810e9a939adce99ec834ebd3d8f3ab69f71
Wesley Gurgel <wesleygurgel27@gmail.com>
2024-12-12 11:54:55 -0300

    feat: pagination, filter and order

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,25 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Task
 from .serializers import TaskSerializer
+
+class TaskPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = TaskPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['completed', 'user']
+    search_fields = ['title', 'description']
+    ordering_fields = ['created_at', 'updated_at', 'title']
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
