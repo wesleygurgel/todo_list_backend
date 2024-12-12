dbf697e43ed802e3d07cafb1cbfac5898370b232
Wesley Gurgel <wesleygurgel27@gmail.com>
2024-12-12 10:30:37 -0300

    fix: table name in Meta Class

--- tasks/models.py
+++ tasks/models.py
@@ -1,13 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='tasks')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        db_table = 'task'
