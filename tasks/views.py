8f8cbf4fc55ac7a4554f236c3050b1a9297a2689
Wesley Gurgel <wesleygurgel27@gmail.com>
2024-12-12 13:31:48 -0300

    feat: MatchType filter - FullMatch or Partial match on querystring params search

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Task
 from .serializers import TaskSerializer
+from .filters import MatchTypeFilter
 
 class TaskPagination(PageNumberPagination):
     page_size = 10
@@ -16,7 +17,7 @@ class TaskViewSet(ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
     pagination_class = TaskPagination
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter, MatchTypeFilter]
     filterset_fields = ['completed', 'user']
     search_fields = ['title', 'description']
     ordering_fields = ['created_at', 'updated_at', 'title']
