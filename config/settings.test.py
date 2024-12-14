6a5be4e42869ec13c730b8ced780c6fd159120fb
Wesley Gurgel <wesleygurgel27@gmail.com>
2024-12-14 17:31:41 -0300

    feat: CI and test settings

---
+++ config/settings.test.py
@@ -0,0 +1,31 @@
+from .settings import *
+
+# Configuração do banco de dados para testes
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "test_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
+# Reduz o log para testes
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
